594e3f45ba3b53cd3e2f3f3784eea0a475cc134c
lucas.desfontaine <lucas.desfontaine1@gmail.com>
2023-04-22 17:13:24 +1100

    feat(dashboard): Add dashboard

---
+++ src/php/dashboard.php
@@ -0,0 +1,19 @@
+<?php
+require_once 'connexion/config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Dashboard</title>
+    <?php
+    include_once path_php . 'include/head.php';
+    ?>
+</head>
+
+<body>
+<?php include_once path_php . 'include/navbar.php' ?>
+
+
+</body>
+</html>
